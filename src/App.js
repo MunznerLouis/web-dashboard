9dd477f06046fa8e04b7d93f03b00675f2051375
Louis Münzner <munzner.louis@gmail.com>
2023-09-05 20:07:03 +0200

    Implemented Routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,25 @@
 // src/App.js
-
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import DonutChartComponent from './components/D3chart';
-import Footer from './footer/footer.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Terms from './pages/Terms-of-services';
+import Policy from './pages/Privacy-policy';
+import Footer from './components/footer/Footer';
+import Services from './pages/services';
 
 function App() {
-  const [chartData, setChartData] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from your endpoint (http://localhost:5000/api/data)
-    fetch('http://localhost:5000/api/data')
-      .then(response => response.json())
-      .then(data => {
-        setChartData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Graph de fou</h1>
-      </header>
-      <main>
-        <div className="App-chart">
-          <DonutChartComponent data={chartData} title="Most picked ADC for each region in percentage" />
-        </div>
-        <Footer/>
-      </main>   
-    </div>
+    <Router> {}
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/terms-of-services" element={<Terms />} />
+          <Route path="/privacy-policy" element={<Policy />} />
+          <Route path="/services" element={<Services />} />
+
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
